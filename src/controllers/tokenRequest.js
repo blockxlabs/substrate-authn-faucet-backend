d83c77ad473a9b3178eecd1fbd745a69270e8d01
kushptlapigarage <kpatel@apigarage.com>
2020-05-28 17:52:24 -0400

    CFD-50 connect-disconnect per request (#13)
    * CFD-50 connect-disconnect per request

--- src/controllers/tokenRequest.js
+++ src/controllers/tokenRequest.js
@@ -49,7 +49,6 @@ export const requestTokens = async (req, res) => {
     if(authType === null || authType === '' || !authType == 'github') throw new Error(ErrorStatus.INVAID_AUTH_TYPE);
     if(ipAddress === null || ipAddress === '' || ipAddress.startsWith('192') ) throw new Error(ErrorStatus.INVALID_IP_ADDRESS);
 
-    await Centrifuge.hasFunds();
     const githubUser = await GithubServices.getGithubUser(github_token);
 
     const isGithubAccountAgeValid = await GithubServices.userReqAfterGithubAccountAge(githubUser);
--- src/services/centrifuge-service.js
+++ src/services/centrifuge-service.js
@@ -1,58 +1,82 @@
 import { ApiPromise, WsProvider, Keyring } from '@polkadot/api';
 import { cryptoWaitReady } from '@polkadot/util-crypto';
 import config from '../config/config';
-import {BN} from 'bn.js';
+import { BN } from 'bn.js';
 import * as ErrorStatus from '../constants/error-status';
 
 const url = config.CFG_NETOWRK_URL;
 const transfer_amount = config.CFG_TRANSFER_AMOUNT;
 
 let pk = null;
-let api = null;
+
+const getConnection = async () => {
+  const provider = new WsProvider(url);
+
+  const api = await ApiPromise.create({
+    provider,
+    types: {
+      // mapping the actual specified address format
+      Address: 'AccountId',
+      // mapping the lookup
+      LookupSource: 'AccountId'
+    }
+  });
+
+  return api;
+};
 
 const startup = async () => {
-  const provider = new WsProvider(url);
   await cryptoWaitReady();
   const keyring = new Keyring();
 
   pk = keyring.addFromUri(config.CFG_HOT_WALLET_SEED, {}, 'sr25519');
-
-  api = await ApiPromise.create({provider,types: {
-    // mapping the actual specified address format
-    Address: 'AccountId',
-    // mapping the lookup
-    LookupSource: 'AccountId'
-  }});
 };
 
 startup();
 
-
-export const hasFunds = async () => {
-  const { balanceBN } = await walletBalance();
+const hasFunds = async (api) => {
+  const { balanceBN } = await walletBalance(api);
   const transferBN = new BN(transfer_amount);
   const requiredAmountBN = transferBN.div(new BN(100)).mul(new BN(110)); // need 10% more
 
-  if(balanceBN.lt(requiredAmountBN)) throw new Error(ErrorStatus.INSUFFICIENT_FUNDS);
+  if (balanceBN.lt(requiredAmountBN))
+    throw new Error(ErrorStatus.INSUFFICIENT_FUNDS);
 };
 
-export const transfer =  async (recipient_address) => {
-  // TODO: Check if the balance is sufficient before transfering the funds.
+export const transfer = async (recipient_address) => {
+  const api = await getConnection();
 
-  const hash = await api.tx.balances.transfer(recipient_address, transfer_amount).signAndSend(pk);
+  try {
+    await hasFunds();
+    const hash = await api.tx.balances
+      .transfer(recipient_address, transfer_amount)
+      .signAndSend(pk);
 
-  return hash;
+    return hash;
+  } finally {
+    await api.disconnect();
+  }
 };
 
-export const walletBalance = async () => {
-  // console.log('The hot wallet is : ', config.CFG_HOT_WALLET_SEED);
+export const walletBalance = async (api) => {
+  let currentApi = null;
+  let disconnect = true;
+  if (api == null) {
+    currentApi = await getConnection();
+    disconnect = true; // remember to disconnect
+  } else {
+    currentApi = api;
+    disconnect = false; // caller will disconnect
+  }
 
-  // TODO: Check if the balance is sufficient before transfering the funds.
-  // let ret = await api.query.system.account(pk.address);
-
-  let { data: { free: previousFree } } = await api.query.system.account(pk.address);
-  // console.log('Address : ', pk.address);
-
-  return { balance: previousFree.toHuman(), balanceBN: previousFree};
+  try {
+    const {
+      data: { free: previousFree }
+    } = await currentApi.query.system.account(pk.address);
+    return { balance: previousFree.toHuman(), balanceBN: previousFree };
+  } finally {
+    if (disconnect === true) {
+      await currentApi.disconnect();
+    }
+  }
 };
-
